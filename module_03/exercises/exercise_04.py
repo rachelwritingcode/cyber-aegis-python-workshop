39659e2fafff333f0ecf0fd4554efbd8d9165e0a
Rachel Wang <21071004+rachelwritingcode@users.noreply.github.com>
2022-08-20 17:39:22 -0400

    Add a new exercise

---
+++ module_03/exercises/exercise_04.py
@@ -0,0 +1,13 @@
+# define subtract function
+def subtract(number_one):
+    number_two = number_one - 2674  
+    # use the return keyword 
+    return number_two
+
+# define two integer variables
+number_one = 4011
+
+# call the add function and pass in two variables as arguments
+flag = str(subtract(number_one))
+# output the value
+print("flag{"+str(flag)+"_phishing}")
