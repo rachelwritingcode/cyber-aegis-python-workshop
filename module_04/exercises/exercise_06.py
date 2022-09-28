af785d049e24bbe8d8e0f643a990ef6afdd48d7f
Rachel Wang <21071004+rachelwritingcode@users.noreply.github.com>
2022-09-27 21:38:05 -0400

    Update exercises

--- module_04/exercises/exercise_06.py
+++ module_04/exercises/exercise_06.py
@@ -4,7 +4,9 @@ import json
 # Find the all the open ports
 nmap = nmap3.Nmap()
 port_results = nmap.scan_top_ports("127.0.0.1")
+# create a json file for the results
 results_file = open("ports_results.json", "w")
-json.dump(port_results, results_file, indent = 6)
+# store the json data into the json file
+json.dump(port_results, results_file, indent = 3)
 results_file.close()
 
--- module_04/exercises/exercise_07.py
+++ module_04/exercises/exercise_07.py
@@ -4,6 +4,8 @@ import json
 # Find out the OS of an IP or url
 nmap = nmap3.Nmap()
 os_results = nmap.nmap_os_detection("127.0.0.1") # Run this as root user
+# create a json file for the results
 results_file = open("network_results.json", "w")
-json.dump(os_results, results_file, indent = 6)
+# store the json data into the json file
+json.dump(os_results, results_file, indent = 3)
 results_file.close()
