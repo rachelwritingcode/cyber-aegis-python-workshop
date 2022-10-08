5e9bf6a5b2a2ff1caea5d0126e840477172ac61f
Rachel Wang <21071004+rachelwritingcode@users.noreply.github.com>
2022-10-08 14:06:19 -0400

    Add with functions example

---
+++ module_03/exercises/with_functions.py
@@ -0,0 +1,11 @@
+def say_hello(person):
+    if person == "pizza":
+        print("you're a pizza, not a human")
+    elif person == "taco":
+        print("you're a taco, not a human")
+    elif person == "jedi":
+        print("you're a jedi...may the force be with you!!!")
+
+say_hello("pizza")
+say_hello("jedi")
+say_hello("taco")
