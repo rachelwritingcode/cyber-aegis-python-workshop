cb4f859c585e197882647165051a021fc98a1b30
Rachel Wang <21071004+rachelwritingcode@users.noreply.github.com>
2022-10-08 14:06:43 -0400

    Add without functions example

---
+++ module_03/exercises/without_functions.py
@@ -0,0 +1,7 @@
+person = "taco"
+if person == "pizza":
+    print("you're a pizza, not a human")
+elif person == "taco":
+    print("you're a taco, not a human")
+elif person == "jedi":
+    print("you're a jedi...may the force be with you!!!")
