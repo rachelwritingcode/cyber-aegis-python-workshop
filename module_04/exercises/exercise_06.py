39948f4f22c1fc9870f8035ee6bfbc8843b9eb59
Rachel Wang <21071004+rachelwritingcode@users.noreply.github.com>
2022-10-09 13:00:17 -0400

    Update exercise_06.py

--- module_04/exercises/exercise_06.py
+++ module_04/exercises/exercise_06.py
@@ -1,12 +1,8 @@
 import nmap3
-import json 
+import json
 
-# Find the all the open ports
-nmap = nmap3.Nmap()
+nmap  = nmap3.Nmap()
 port_results = nmap.scan_top_ports("127.0.0.1")
-# create a json file for the results
-results_file = open("ports_results.json", "w")
-# store the json data into the json file
-json.dump(port_results, results_file, indent = 3)
-results_file.close()
 
+with open('port_results.json', 'w', encoding='utf-8') as f:
+    f.write(json.dumps(port_results, indent=3))
