22b875e3e9fb682a7a7b4aa909af50170855e16a
Rachel Wang <21071004+rachelwritingcode@users.noreply.github.com>
2022-10-09 13:02:40 -0400

    Update exercise_07.py

--- module_04/exercises/exercise_07.py
+++ module_04/exercises/exercise_07.py
@@ -4,8 +4,5 @@ import json
 # Find out the OS of an IP or url
 nmap = nmap3.Nmap()
 os_results = nmap.nmap_os_detection("127.0.0.1") # Run this as root user
-# create a json file for the results
-results_file = open("network_results.json", "w")
-# store the json data into the json file
-json.dump(os_results, results_file, indent = 3)
-results_file.close()
+with open('os_results.json', 'w', encoding='utf-8') as f:
+    f.write(json.dumps(os_results, indent=3))
