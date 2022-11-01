6b349f6bd58948974f5c7d091922cbe82cb5fef0
rachelwritingcode <21071004+rachelwritingcode@users.noreply.github.com>
2022-11-01 19:25:27 -0400

    Update exercise files

--- module_04/exercises/exercise_06.py
+++ module_04/exercises/exercise_06.py
@@ -4,5 +4,5 @@ import json
 nmap  = nmap3.Nmap()
 port_results = nmap.scan_top_ports("127.0.0.1")
 
-with open('port_results.json', 'w', encoding='utf-8') as f:
+with open('port_results.json', 'w') as f:
     f.write(json.dumps(port_results, indent=3))
--- module_04/exercises/exercise_07.py
+++ module_04/exercises/exercise_07.py
@@ -4,5 +4,5 @@ import json
 # Find out the OS of an IP or url
 nmap = nmap3.Nmap()
 os_results = nmap.nmap_os_detection("127.0.0.1") # Run this as root user
-with open('os_results.json', 'w', encoding='utf-8') as f:
+with open('os_results.json', 'w') as f:
     f.write(json.dumps(os_results, indent=3))
